8fde2ef437794ec20728df8442e0e1cc652ddceb
Charan-Epuri <Charan.Epuri1@marist.edu>
2024-08-30 16:16:27 -0400

    Initial Commit

---
+++ springboot-starter-apis/src/main/java/com/codingshuttle/charanTutorialStarterApis/springboot_starter_apis/SpringbootStarterApisApplication.java
@@ -0,0 +1,13 @@
+package com.codingshuttle.charanTutorialStarterApis.springboot_starter_apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStarterApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStarterApisApplication.class, args);
+	}
+
+}
